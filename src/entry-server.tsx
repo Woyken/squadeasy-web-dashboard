34736831dfcca5afc679552a1a94370465795530
Woyken <13166802+Woyken@users.noreply.github.com>
2024-06-05 14:44:44 +0300

    use theme dark

--- src/entry-server.tsx
+++ src/entry-server.tsx
@@ -5,6 +5,7 @@ export default createHandler(() => (
     <StartServer
         document={({ assets, children, scripts }) => (
             <html
+                data-theme="dark"
                 lang="en"
                 class="sl-theme-dark"
                 style={{ "color-scheme": "dark" }}

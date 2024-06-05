d879a41c674a183218126bc9968faca70002a178
Woyken <13166802+Woyken@users.noreply.github.com>
2024-06-05 14:34:20 +0300

    add daisyUI countdown

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -52,13 +52,35 @@ export default function Home() {
             .padStart(2, "0")
     );
     return (
-        <main>
+        <main class="flex flex-col items-center">
             <Title>SquadEasy</Title>
             <h1>Countdown</h1>
-            <h2>
-                {diffMs() < 0 ? "-" : ""} {dLeft()}d {hLeft()}h:{mLeft()}m:
-                {sLeft()}s
-            </h2>
+            <div class="grid grid-flow-col gap-5 text-center auto-cols-max">
+                <div class="flex flex-col p-2 bg-neutral rounded-box text-neutral-content">
+                    <span class="countdown font-mono text-5xl">
+                        <span style={`--value:${dLeft()};`}></span>
+                    </span>
+                    days
+                </div>
+                <div class="flex flex-col p-2 bg-neutral rounded-box text-neutral-content">
+                    <span class="countdown font-mono text-5xl">
+                        <span style={`--value:${hLeft()};`}></span>
+                    </span>
+                    hours
+                </div>
+                <div class="flex flex-col p-2 bg-neutral rounded-box text-neutral-content">
+                    <span class="countdown font-mono text-5xl">
+                        <span style={`--value:${mLeft()};`}></span>
+                    </span>
+                    min
+                </div>
+                <div class="flex flex-col p-2 bg-neutral rounded-box text-neutral-content">
+                    <span class="countdown font-mono text-5xl">
+                        <span style={`--value:${sLeft()};`}></span>
+                    </span>
+                    sec
+                </div>
+            </div>
         </main>
     );
 }

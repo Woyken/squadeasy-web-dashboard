5d6004b1b8f6c54e2ed00837cfdfa1a584964f21
Woyken <13166802+Woyken@users.noreply.github.com>
2024-06-10 13:04:56 +0300

    Remove data migration code

--- src/components/TeamScoreTracker.tsx
+++ src/components/TeamScoreTracker.tsx
@@ -168,103 +168,9 @@ export function useTeamsUsersScore() {
 }
 
 function TeamUsersScoreTracker(props: ParentProps<{ teamsIds: string[] }>) {
-    const users = useUsersTokens();
-    const firstUserId = createMemo(() => Array.from(users().tokens.keys())[0]);
-    const getToken = useGetUserToken(firstUserId);
-    const queriesOptions = createMemo(() =>
-        props.teamsIds.map((teamId) =>
-            teamQueryOptions(
-                () => teamId,
-                getToken,
-                () => true,
-                2 * 60 * 60 * 1000,
-                true,
-            ),
-        ),
-    );
-    const teamsQueries = createQueries(() => ({
-        queries: queriesOptions(),
-    }));
-
     const [localData, setLocalData] = createSignal<
         Record<string, { users: Record<string, number>; timestamp: number }[]>
     >({});
-
-    const [migrationDone, setMigrationDone] = createSignal(false);
-    const [dataToMigrate, setDataToMigrate] =
-        createSignal<{ users: Record<string, number>; timestamp: number }[]>();
-    createEffect(() => {
-        // Migrate old data
-        teamsStorage
-            .getItem<
-                { users: Record<string, number>; timestamp: number }[]
-            >("teamUserPoints")
-            .then((oldData) => {
-                if (!oldData) return setMigrationDone(true);
-                setDataToMigrate(oldData);
-            });
-    });
-
-    createEffect(() => {
-        if (migrationDone()) return;
-        const dm = dataToMigrate();
-        if (!dm) return;
-        if (dm.length === 0) {
-            teamsStorage.removeItem("teamUserPoints");
-            setMigrationDone(true);
-            return;
-        }
-
-        const foundData = teamsQueries
-            .map((x) => {
-                if (!x.data) return;
-                const userId = x.data.users[0]?.id;
-                if (!userId) return;
-                return {
-                    userId,
-                    teamId: x.data.id,
-                };
-            })
-            .filter((x) => !!x)
-            .map((x) => x as NonNullable<typeof x>)
-            .map((x) => {
-                return {
-                    teamId: x.teamId,
-                    dataForTeam: dm.filter(
-                        (dmus) =>
-                            !!Object.keys(dmus.users).find(
-                                (u) => u === x.userId,
-                            ),
-                    ),
-                };
-            });
-        if (foundData.length > 0) {
-            foundData.forEach((x) => {
-                teamsStorage.setItem(
-                    `teamUserPoints-${x.teamId}`,
-                    x.dataForTeam,
-                );
-            });
-            const newDataToMigrate = dm.filter(
-                (d) =>
-                    !foundData.find(
-                        (x) => !!x.dataForTeam.find((y) => y === d),
-                    ),
-            );
-            setDataToMigrate((old) =>
-                !old || old.length === newDataToMigrate.length
-                    ? old
-                    : newDataToMigrate,
-            );
-        }
-
-        if (!teamsQueries.some((q) => !q.data)) {
-            // All queries are completed, mark migration as completed now
-            teamsStorage.removeItem("teamUserPoints");
-            setMigrationDone(true);
-            setDataToMigrate(undefined);
-        }
-    });
 
     const usersIds = createMemo(() => {
         const ld = localData();
@@ -280,17 +186,15 @@ function TeamUsersScoreTracker(props: ParentProps<{ teamsIds: string[] }>) {
 
     return (
         <teamUsersCtx.Provider value={() => localData() ?? []}>
-            <Show when={migrationDone()}>
-                <For each={props.teamsIds}>
-                    {(teamId) => (
-                        <TrackTeamUsersScore
-                            teamId={teamId}
-                            parentData={localData}
-                            setParentData={setLocalData}
-                        />
-                    )}
-                </For>
-            </Show>
+            <For each={props.teamsIds}>
+                {(teamId) => (
+                    <TrackTeamUsersScore
+                        teamId={teamId}
+                        parentData={localData}
+                        setParentData={setLocalData}
+                    />
+                )}
+            </For>
             <UsersStatisticsTracker usersIds={usersIds()}>
                 {props.children}
             </UsersStatisticsTracker>
@@ -381,24 +285,6 @@ export function useUserStatistics() {
 }
 
 function UsersStatisticsTracker(props: ParentProps<{ usersIds: string[] }>) {
-    const users = useUsersTokens();
-    const firstUserId = createMemo(() => Array.from(users().tokens.keys())[0]);
-    const getToken = useGetUserToken(firstUserId);
-    const queriesOptions = createMemo(() =>
-        props.usersIds.map((x) =>
-            userStatisticsQueryOptions(
-                () => x,
-                getToken,
-                () => true,
-                2 * 60 * 60 * 1000,
-                true,
-            ),
-        ),
-    );
-    const usersStatisticsQueries = createQueries(() => ({
-        queries: queriesOptions(),
-    }));
-
     const [localData, setLocalData] = createSignal<
         Record<
             string,
@@ -409,106 +295,16 @@ function UsersStatisticsTracker(props: ParentProps<{ usersIds: string[] }>) {
         >
     >({});
 
-    const [migrationDone, setMigrationDone] = createSignal(false);
-    const [dataToMigrate, setDataToMigrate] = createSignal<
-        {
-            userId: string;
-            activities: Record<string, { value: number; points: number }>;
-            timestamp: number;
-        }[]
-    >();
-    createEffect(() => {
-        // Migrate old data
-        teamsStorage
-            .getItem<
-                {
-                    userId: string;
-                    activities: Record<
-                        string,
-                        { value: number; points: number }
-                    >;
-                    timestamp: number;
-                }[]
-            >("userStatistics")
-            .then((oldData) => {
-                if (!oldData) return setMigrationDone(true);
-                setDataToMigrate(oldData);
-            });
-    });
-
-    createEffect(() => {
-        if (migrationDone()) return;
-        const dm = dataToMigrate();
-        if (!dm) return;
-        if (dm.length === 0) {
-            teamsStorage.removeItem("userStatistics");
-            setMigrationDone(true);
-            return;
-        }
-
-        const foundData = usersStatisticsQueries
-            .map((x) => {
-                if (!x.data) return;
-                const userId = x.data.id;
-                if (!userId) return;
-                return userId;
-            })
-            .filter((x) => !!x)
-            .map((x) => x as NonNullable<typeof x>)
-            .map((x) => {
-                return {
-                    userId: x,
-                    dataForUser: dm.filter((dmus) => dmus.userId === x),
-                };
-            });
-        if (foundData.length > 0) {
-            foundData.forEach((x) => {
-                teamsStorage.setItem<
-                    {
-                        activities: Record<
-                            string,
-                            { value: number; points: number }
-                        >;
-                        timestamp: number;
-                    }[]
-                >(
-                    `userStatistics-${x.userId}`,
-                    x.dataForUser.map((x) => ({
-                        timestamp: x.timestamp,
-                        activities: x.activities,
-                    })),
-                );
-            });
-            const newDataToMigrate = dm.filter(
-                (d) => !foundData.find((x) => x.userId === d.userId),
-            );
-            setDataToMigrate((old) =>
-                !old || old.length === newDataToMigrate.length
-                    ? old
-                    : newDataToMigrate,
-            );
-        }
-
-        if (!usersStatisticsQueries.some((q) => !q.data)) {
-            // All queries are completed, mark migration as completed now
-            teamsStorage.removeItem("userStatistics");
-            setMigrationDone(true);
-            setDataToMigrate(undefined);
-        }
-    });
-
     return (
         <userStatisticsCtx.Provider value={localData}>
-            <Show when={migrationDone()}>
-                <For each={props.usersIds}>
-                    {(userId) => (
-                        <TrackUserStatistics
-                            userId={userId}
-                            setParentData={setLocalData}
-                        />
-                    )}
-                </For>
-            </Show>
+            <For each={props.usersIds}>
+                {(userId) => (
+                    <TrackUserStatistics
+                        userId={userId}
+                        setParentData={setLocalData}
+                    />
+                )}
+            </For>
             {props.children}
         </userStatisticsCtx.Provider>
     );
